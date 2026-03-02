5e8344445a164362e3b3a949feb979634eac5602
lou-delmay <loganoracio@gmail.com>
2026-03-02 02:38:19 +0100

    added dice sfx and deck shuffling sfx

--- Assets/Scripts/Card/CardDeck.cs
+++ Assets/Scripts/Card/CardDeck.cs
@@ -25,6 +25,13 @@ public class CardDeck : MonoBehaviour
     private Coroutine typewriterCoroutine;
     private string currentFullText = "";
 
+    private AudioManager audioManager;
+
+    void Awake()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
+
     private void fisherYates(List<int> array)
     {
         System.Random rng = new System.Random();
@@ -78,6 +85,7 @@ public class CardDeck : MonoBehaviour
 
         for(int k = 0; k < 4; k++)
         {
+            audioManager.SfxSource.PlayOneShot(audioManager.SfxCards[k%3], 1.0f);
             List<int> newids = new List<int>();
             for(int i = 0; i < cardsPerDeck; i++)
             {
